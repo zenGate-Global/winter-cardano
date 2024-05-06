c6a6e1c5198fbd2bc8d75e042428a9089a8b263a
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-05-06 18:14:24 -0400

    add asset utxos endpoint

--- src/models/koios.types.ts
+++ src/models/koios.types.ts
@@ -6,3 +6,37 @@ export type ScriptInfo = {
   bytes: string;
   size: number;
 };
+
+export interface UtxoInfo {
+  tx_hash: string;
+  tx_index: number;
+  address: string;
+  value: string;
+  stake_address: string | null;
+  payment_cred: string | null;
+  epoch_no: number;
+  block_height: number | null;
+  block_time: number;
+  datum_hash: string | null;
+  inline_datum: {
+    bytes: string;
+    value: any | null;
+  };
+  reference_script: {
+    hash: string;
+    size: number;
+    type: string;
+    bytes: string;
+    value: any | null;
+  } | null;
+  asset_list:
+    | {
+        decimals: number;
+        quantity: string;
+        policy_id: string;
+        asset_name: string | null;
+        fingerprint: string;
+      }[]
+    | null;
+  is_spent: boolean;
+}
