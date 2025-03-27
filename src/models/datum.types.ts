bc05a879bdd841d42230afc6b858bfc8f0066d3b
Luca D'Angelo <ldgaetano@protonmail.com>
2025-03-27 18:41:51 +0900

    Refactor spend transaction.

--- src/models/datum.types.ts
+++ src/models/datum.types.ts
@@ -1,10 +1,19 @@
-import { Data as TData } from '../data';
+// import { Data as TData } from '../data';
 
-export const ObjectDatum = TData.Object({
-  protocol_version: TData.Integer(),
-  data_reference: TData.Bytes(),
-  event_creation_info: TData.Bytes(),
-  signers: TData.Array(TData.Bytes())
-});
+import { ByteString, Integer, List, PubKeyHash } from '@meshsdk/core';
 
-export type ObjectDatum = TData.Static<typeof ObjectDatum>;
+// export const ObjectDatum = TData.Object({
+//   protocol_version: TData.Integer(),
+//   data_reference: TData.Bytes(),
+//   event_creation_info: TData.Bytes(),
+//   signers: TData.Array(TData.Bytes())
+// });
+
+// export type ObjectDatum = TData.Static<typeof ObjectDatum>;
+
+export type ObjectDatum = {
+  protocol_version: Integer;
+  data_reference_hex: ByteString;
+  event_creation_info_tx_hash: ByteString;
+  signers_pk_hash: List<PubKeyHash>;
+};
