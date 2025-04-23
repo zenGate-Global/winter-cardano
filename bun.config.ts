cc66133db69e8060cdeef53d4019595ef7e238a9
Luca D'Angelo <ldgaetano@protonmail.com>
2025-04-23 16:22:10 +0900

    fix(build): use cjs and mjs extensions

--- bun.config.ts
+++ bun.config.ts
@@ -1,32 +1,43 @@
 import dts from "bun-plugin-dts";
 
-const ecmascript = await Bun.build({
-	entrypoints: ["./src/index.ts"],
-	outdir: "./dist/esm",
-	format: "esm",
-	target: "node",
-	sourcemap: "none",
-	minify: false,
-	splitting: false,
-	external: undefined,
-	plugins: [dts()],
-});
+try {
+	const ecmascript = await Bun.build({
+		entrypoints: ["./src/index.ts"],
+		outdir: "./dist",
+		naming: {
+			entry: "index.mjs",
+		},
+		format: "esm",
+		target: "node",
+		sourcemap: "none",
+		minify: false,
+		splitting: false,
+		external: undefined,
+		plugins: [dts()],
+	});
 
-if (ecmascript.success) {
-	console.log("ECMAScript build complete.");
-}
+	const commonjs = await Bun.build({
+		entrypoints: ["./src/index.ts"],
+		outdir: "./dist",
+		naming: {
+			entry: "index.cjs",
+		},
+		format: "cjs",
+		target: "node",
+		sourcemap: "none",
+		minify: false,
+		splitting: false,
+		external: undefined,
+	});
 
-const commonjs = await Bun.build({
-	entrypoints: ["./src/index.ts"],
-	outdir: "./dist/cjs",
-	format: "cjs",
-	target: "node",
-	sourcemap: "none",
-	minify: false,
-	splitting: false,
-	external: undefined,
-});
+	if (ecmascript.success) {
+		console.log("ECMAScript build complete.");
+	}
 
-if (commonjs.success) {
-	console.log("CommonJS build complete.");
+	if (commonjs.success) {
+		console.log("CommonJS build complete.");
+	}
+} catch (e) {
+	const error = e as AggregateError;
+	console.error("Build Failed:", error);
 }
